73586048832c8eccfe1abf98b756f303777b4fe1
Dang Phuong Nam <67249286+phuonam248@users.noreply.github.com>
2021-09-19 21:20:30 +0700

    commit lan 4

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManagers : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
