1695294911f9a8d89767f1e1e61b9c062cb9ba5a
Dang Phuong Nam <67249286+phuonam248@users.noreply.github.com>
2021-09-19 21:18:56 +0700

    Commit lan 3

--- Assets/Scripts/Interfaces/IClickable.cs
+++ Assets/Scripts/Interfaces/IClickable.cs
@@ -2,26 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< Updated upstream
-public class IClickable : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-=======
 public interface IClickable {
     void OnLeftClick();
     void OnRightClick();
     
     void OnHoverEnter();
     void OnHoverExist();
->>>>>>> Stashed changes
 }
